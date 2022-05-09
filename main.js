dc032c2e6640193d719e8c8f72e8a0c6e0c8ea34
steve <steve.nguyen2013@gmail.com>
2022-05-08 17:08:42 -0700

    complete assignment

--- main.js
+++ main.js
@@ -3,10 +3,58 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//second step is to recognize the JS events
+//third step communicate with the server. 
+
+
+/*function hideError(){
+  document.getElementById("modal").hidden = true
+ document.getElementById("modal-message").hidden = true
+}
+setTimeout(()=>hideError(),3000)
+
+mimicServerCall()
+.then((response)=>{return response})
+.catch((error)=>{console.log(error)})
+
+
+const allHearts = document.querySelectorAll(".like-glyph");
+for (const change of allHearts){
+  change.addEventListener("click",mimicServerCall);
+}
+*/
+
+
+function handleEvent(event){
+  let like = event.target
+  mimicServerCall()
+.then(()=>{
+
+  if(like.innerHTML === EMPTY_HEART){
+    like.innerHTML = FULL_HEART
+  } else { like.innerHTML = EMPTY_HEART}
+  
+})
+
+.catch(function(error){
+  const modal = document.querySelector(".hidden")
+  modal.className = "hidden"
+  setTimeout(function(){
+    error.modal
+  },3000)
+})
+}
+const allHearts = document.querySelectorAll(".like-glyph")
+for (const change of allHearts){
+  change.addEventListener("click", handleEvent)
+}
 
 
 
 
+
+
+//
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
